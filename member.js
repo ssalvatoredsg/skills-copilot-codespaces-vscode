d8794e9938e26953c7b371dd6cb13887f77c982a
Steve Salvatore <60185454+ssalvatoredsg@users.noreply.github.com>
2023-07-31 12:56:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberLength = skills;
+}
